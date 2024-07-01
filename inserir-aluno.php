20a931addb4f804a8356a54dd6bc2b89b290f4f1
luccas-arruda <luccasarruda@hotmail.com.br>
2024-07-01 13:10:46 -0300

    feat: adicionar arquivos para criação de arrays de alunos do banco de dados

--- inserir-aluno.php
+++ inserir-aluno.php
@@ -7,7 +7,7 @@ require_once 'vendor/autoload.php';
 $caminhoBanco = __DIR__ . '/banco.sqlite';
 $pdo = new PDO("sqlite:$caminhoBanco");
 
-$student = new Student(null, 'Luccas Arruda', new DateTimeImmutable('2004-11-26'));
+$student = new Student(null, 'Maria Fernanda', new DateTimeImmutable('1982-11-30'));
 $studentName = $student->name();
 $studentBirthDate = $student->birthDate()->format('Y-m-d');
 
--- lista-alunos.php
+++ lista-alunos.php
@@ -1,11 +1,22 @@
 <?php
 
+use Alura\Pdo\Domain\Model\Student;
+
 require_once 'vendor/autoload.php';
 
 $databasePath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO("sqlite:$databasePath");
 
 $statement = $pdo->query('SELECT * FROM students');
-$studentsList = $statement->fetchAll();
+$studentsDataList = $statement->fetchAll(PDO::FETCH_ASSOC);
+$studentsList = [];
 
-echo $studentsList[0]['name'];
+foreach ($studentsDataList as $studentData) {
+    $studentsList[] = new Student(
+        $studentData['id'],
+        $studentData['name'],
+        new DateTimeImmutable($studentData['birth_date'])
+    );
+}
+
+var_dump($studentsList);
