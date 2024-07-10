70d4263e54fcc3b8f8a5f354ea135861a00f4dcd
luccas-arruda <luccasarruda@hotmail.com.br>
2024-07-10 20:00:01 -0300

    chore: adicionar pasta de insfraestrutura

--- remover-aluno.php
+++ remover-aluno.php
@@ -10,6 +10,8 @@ $pdo = new PDO("sqlite:$databasePath");
 $sqlDelete = 'DELETE FROM students WHERE ID = :id;';
 $preparedStatement = $pdo->prepare($sqlDelete);
 $preparedStatement->bindValue(':id', 4, PDO::PARAM_INT);
+
 // $preparedStatement->bindValue(':id', 4, PDO::PARAM_INT); 
 // é possível utilizar o mesmo preparedStatement mais de uma vez, apenas trocando os valores dos parâmetros
+
 var_dump($preparedStatement->execute());
---
+++ src/Infrastructure/Persistence/ConnectionCreator.php
@@ -0,0 +1,9 @@
+<?php   
+
+namespace Alura\Pdo\Infrastructure\Persistence;
+
+class ConnectionCreator {
+    public function create(){
+        
+    }]
+}
