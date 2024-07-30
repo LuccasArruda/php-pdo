8a127da960c4970376ed01f9c5c42f5b2a0d96c8
luccas-arruda <luccasarruda@hotmail.com.br>
2024-07-30 20:05:35 -0300

    faet: adicionar tratamento de exceções

--- criar-turma.php
+++ criar-turma.php
@@ -11,15 +11,17 @@ $studentRepository = new PdoStudentRepository($connection);
 
 $connection->beginTransaction(); 
 
-$student = new Student(null, 'Luccas', new DateTimeImmutable('2004-11-26'));
-$studentRepository->save($student);
+try {
+    $student = new Student(null, 'Luccas', new DateTimeImmutable('2004-11-26'));
+    $studentRepository->save($student);
 
-$newStudent = new Student(null, 'Jeferson Jovem', new DateTimeImmutable('2006-11-23'));
-$studentRepository->save($newStudent);
+    $newStudent = new Student(null, 'Jeferson Jovem', new DateTimeImmutable('2006-11-23'));
+    $studentRepository->save($newStudent);
+    
+    $connection->commit();
 
-if (($student->id() == null) && ($newStudent->id() == null)){
+} catch(RuntimeException $error) {
+    echo $error->getMessage();
     $connection->rollBack();
-    echo "Os alunos estão sem código!";
 }
 
-$connection->commit();
